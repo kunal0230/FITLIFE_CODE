37bb760dca417254e6ff43c9dbf139875573a7f2
kunal0230 <109679265+kunal0230@users.noreply.github.com>
2024-04-02 12:27:42 +0530

    Add files via upload

---
+++ admin/dashboard.php
@@ -0,0 +1,121 @@
+<?php
+
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>dashboard</title>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+
+<section class="dashboard">
+
+   <h1 class="heading">dashboard</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>welcome admin!</h3>
+         <p><?= $fetch_profile['name']; ?></p>
+         <a href="update_profile.php" class="btn">update profile</a>
+      </div>
+
+
+
+
+
+      <div class="box">
+         <?php
+            $select_orders = $conn->prepare("SELECT * FROM `orders`");
+            $select_orders->execute();
+            $number_of_orders = $select_orders->rowCount()
+         ?>
+         <h3><?= $number_of_orders; ?></h3>
+         <p>Plan details</p>
+         <a href="placed_orders.php" class="btn">Plan Configuration</a>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_products = $conn->prepare("SELECT * FROM `products`");
+            $select_products->execute();
+            $number_of_products = $select_products->rowCount()
+         ?>
+         <h3><?= $number_of_products; ?></h3>
+         <p>Add or Remove excercises</p>
+         <a href="products.php" class="btn">See all excercises</a>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_users = $conn->prepare("SELECT * FROM `users`");
+            $select_users->execute();
+            $number_of_users = $select_users->rowCount()
+         ?>
+         <h3><?= $number_of_users; ?></h3>
+         <p>normal users</p>
+         <a href="users_accounts.php" class="btn">see users</a>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_admins = $conn->prepare("SELECT * FROM `admins`");
+            $select_admins->execute();
+            $number_of_admins = $select_admins->rowCount()
+         ?>
+         <h3><?= $number_of_admins; ?></h3>
+         <p>moderators</p>
+         <a href="admin_accounts.php" class="btn">Add moderators</a>
+      </div>
+
+      <div class="box">
+         <?php
+            $select_messages = $conn->prepare("SELECT * FROM `messages`");
+            $select_messages->execute();
+            $number_of_messages = $select_messages->rowCount()
+         ?>
+         <h3><?= $number_of_messages; ?></h3>
+         <p>new messages</p>
+         <a href="messages.php" class="btn">see messages</a>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="../js/admin_script.js"></script>
+   
+</body>
+</html>
